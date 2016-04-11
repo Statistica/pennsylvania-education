2dfff84235c3b03601c609fa1ee1acec3f08edbe
Jonathan Saewitz <busterroni11@gmail.com>
2016-04-11 18:06:03 -0400

    Update counties_avg_difference.py

--- counties_avg_difference.py
+++ counties_avg_difference.py
@@ -15,12 +15,11 @@ for county in counties:
 	expected_sat=0.007214185143521261*county['per_capita_income'] + 1278.0473160791375 #calculate the expected sat
 	difference=county['avg_sat']-expected_sat
 	county.update({'expected_sat': expected_sat, 'difference': difference}) #update the county
-	del county['avg_sat'], county['per_capita_income'] #delete the avg_sat and per_capita_income from the
-													   #county, we don't need them anymore
+	del county['per_capita_income'] #delete the per_capita_income from the county, we don't need it anymore
 
 f=open('counties_avg_sat_difference.csv', 'w') #save the counties to a .csv
 w=csv.writer(f)
-w.writerow(["county", "expected sat score", "difference from expected"])
+w.writerow(["county", "average sat score", "expected sat score", "difference from expected"])
 for c in sorted(counties, key=operator.itemgetter('difference'), reverse=True):
-	w.writerow([c['county'], round(c['expected_sat'], 2), round(c['difference'], 2)])
+	w.writerow([c['county'], c['avg_sat'], round(c['expected_sat'], 2), round(c['difference'], 2)])
 f.close()
