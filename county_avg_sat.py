7f2391cb8ab98cf8c58a04026186f6feb80a118e
Jonathan Saewitz <busterroni11@gmail.com>
2016-04-11 16:27:03 -0400

    Update county_avg_sat.py

--- county_avg_sat.py
+++ county_avg_sat.py
@@ -20,8 +20,8 @@ with open('pa_schools.csv', 'r') as f: #add all of the schools to the 'schools' 
 
 schools_sats=[]
 
-with open('pa_avg_sat.csv', 'r') as f: #add each high school's sat score
-				       #pa_avg_sat.csv from: http://www.education.pa.gov/K-12/Assessment%20and%20Accountability/Pages/SAT-and-ACT.aspx (Public School SAT Scores 2015)
+with open('pa_sat_scores.csv', 'r') as f: #add each high school's sat score
+					  #pa_sat_scores.csv from: http://www.education.pa.gov/K-12/Assessment%20and%20Accountability/Pages/SAT-and-ACT.aspx (Public School SAT Scores 2015)
 	reader=csv.reader(f)
 	for i in range(8): #skip header rows
 		next(reader)
@@ -41,28 +41,33 @@ grouped=collections.defaultdict(list) #created a defaultdict
 for county in schools_sats:
 	grouped[county['county']].append(county['score']) #append the scores to counties in the defaultdict
 
-district_avg_scores=[]
+county_avg_scores=[]
 for county, scores in grouped.iteritems(): #get the average scores for each county
-	district_avg_scores.append({'county': county, 'avg': sum(scores)/len(scores)})
+	county_avg_scores.append({'county': county, 'avg': sum(scores)/len(scores)})
 
 #get each county's per capita income
-for district_avg_score in district_avg_scores: #loop through every county's average sat scores
+for county_avg_score in county_avg_scores: #loop through every county's average sat scores
 	with open('pa_avg_income.csv', 'r') as f: #pa_avg_income.csv from: https://en.wikipedia.org/wiki/List_of_Pennsylvania_counties_by_per_capita_income#Pennsylvania_counties_ranked_by_per_capita_income (from US Census Bureau)
 		reader=csv.reader(f)
 		for row in reader: #loop through every county average income
-			if district_avg_score['county']==row[1]: #row[1] is the county name
-				mean_income=int(row[2].replace("$", "").replace(",", "")) #format money (e.g. "$41,251"->41251)
-				district_avg_score.update({'mean_income': mean_income})
+			if county_avg_score['county']==row[1]: #row[1] is the county name
+				per_capita_income=int(row[2].replace("$", "").replace(",", "")) #format money (e.g. "$41,251"->41251)
+				county_avg_score.update({'per_capita_income': per_capita_income})
 				break #if we already found the county's income, no need to keep looping
 
 sats=[]
 incomes=[]
 names=[]
 
-for d in district_avg_scores:
-	sats.append(d['avg'])
-	incomes.append(d['mean_income'])
-	names.append(d['county'])
+f=open('counties_avg_sat.csv', 'w')
+w=csv.writer(f)
+w.writerow(["county", "average sat score", "per capita income"])
+for c in county_avg_scores:
+	sats.append(c['avg'])
+	incomes.append(c['per_capita_income'])
+	names.append(c['county'])
+	w.writerow([c['county'], c['avg'], c['per_capita_income']])
+f.close()
 
 trace=go.Scatter(
 	x=incomes,
@@ -72,4 +77,4 @@ trace=go.Scatter(
 )
 data=[trace]
 fig=go.Figure(data=data)
-plotly.plot(fig) #plot the scatter plot!
+# plotly.plot(fig) #plot the scatter plot!
